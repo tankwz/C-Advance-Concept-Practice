377091d6240dc58704c38fdad0f1d89474011d24
tankw <tantk113@gmail.com>
2023-10-22 18:17:00 +0700

    generic filter using lambda and Func<T,bool>

--- C# Advance Concept/LambdasAndCollections.cs
+++ C# Advance Concept/LambdasAndCollections.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,23 +10,35 @@ namespace C__Advance_Concept
     internal class LambdasAndCollections
     {
         record Hero(string Name, string HeroClass, int str);
+     //   delegate bool Filtder<T>(T h);
+
 
         public static void Main(string[] args) {
-            List<Hero> FilterHeroesMoreThan15str(List<Hero> heroes)
+            IEnumerable<T> Filter<T>(IEnumerable<T> items, Func<T, bool> f)
             {
-
-                return heroes.Where(a => a.str > 15).ToList();
+                foreach (var item in items)
+                {
+                    if (f(item))
+                    {
+                        yield return item;
+                    }
+                }
             }
+
+
             var heroes = new List<Hero>
             {
                 new("Ken", "Barbarian", 23),
                 new("Kin", "Cleric", 15),
+                new("Kin", "Cleric", 12),
                 new("Sin", "Wreck", 1)
             };
             //alt f12 = quick peek
-            var hero = FilterHeroesMoreThan15str(heroes);
 
-            var a = string.Join(",", heroes);
+
+            //   var hero = FilterHeroesMoreThan15str(heroes);
+            var hero = Filter(heroes, a => a.str > 15);
+            var a = string.Join(",", hero);
 
             Console.WriteLine(a);
         }
