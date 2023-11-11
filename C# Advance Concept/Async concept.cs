3c8f2d37ac542f17c05b9ef2ca34cc14b018a2fe
tankw <tantk113@gmail.com>
2023-11-11 18:19:58 +0700

    Parallel programming definition

---
+++ C# Advance Concept/Async concept.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Advance_Concept
+{
+    internal class Async_concept
+    {
+        public void note()
+        {
+            /* 
+             
+            A thread is an instance of work; 
+            it's the smallest unit of execution within a process
+
+            A process can consist of multiple threads, 
+            and they share the same process resources
+            
+            A computer can run multiple process due to the operating system scheduler
+            
+            Parallel programming is when you distribute the calculation problem into multiple part 
+            and you run them in parallel
+            it means you have some cpu bound you would like to do in parallel at the same point in time, 
+            multiple calculation going on at the same time
+
+             */
+
+            //For parallel programming you need multiple cpu core
+            //For async programming you DO NOT
+
+            /*
+             
+
+             
+             */
+
+
+
+
+
+        }
+    }
+}
